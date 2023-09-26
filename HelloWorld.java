697072258e8b4f0c3a880450f3bd0d573c5550d0
bharatNakka <nbarath2009@gmail.com>
2023-09-26 10:24:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+
+public class SecureCodeExample {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String prompt = getPrompt();
+        System.out.print(prompt);
+        
+        String name = scanner.nextLine();
+
+       
+        scanner.close();
+    }
+
+   
 }
